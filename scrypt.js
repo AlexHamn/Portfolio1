51221183914ce4fac6635df5514e099e3e6e64c6
AlexHamn <agalexandro16@gmail.com>
2021-08-02 11:32:55 -0500

    add mobile menu skeleton

---
+++ scrypt.js
@@ -0,0 +1,8 @@
+let mobileMenuButton = document.getElementById('mobileMenu');
+let header = document.getElementsByTagName('head');
+
+mobileMenuButton.addEventListener('click', mobileMenu());
+
+function mobileMenu() {
+    header.
+}
